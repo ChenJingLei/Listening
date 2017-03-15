4363be14b91f7afb79fb0ceaa0d948387b818227
cjl20 <cjl20@DESKTOP-G1CE0R9>
2017-03-15 14:31:37 +0800

    init project

---
+++ Listening/Ribbon1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Listening
+{
+    partial class Ribbon1 : Microsoft.Office.Tools.Ribbon.RibbonBase
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        public Ribbon1()
+            : base(Globals.Factory.GetRibbonFactory())
+        {
+            InitializeComponent();
+        }
+
+        /// <summary> 
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region 组件设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tab1 = this.Factory.CreateRibbonTab();
+            this.group1 = this.Factory.CreateRibbonGroup();
+            this.comboBox1 = this.Factory.CreateRibbonComboBox();
+            this.button1 = this.Factory.CreateRibbonButton();
+            this.editBox1 = this.Factory.CreateRibbonEditBox();
+            this.tab1.SuspendLayout();
+            this.group1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab1
+            // 
+            this.tab1.ControlId.ControlIdType = Microsoft.Office.Tools.Ribbon.RibbonControlIdType.Office;
+            this.tab1.Groups.Add(this.group1);
+            this.tab1.Label = "TabAddIns";
+            this.tab1.Name = "tab1";
+            // 
+            // group1
+            // 
+            this.group1.Items.Add(this.comboBox1);
+            this.group1.Items.Add(this.editBox1);
+            this.group1.Items.Add(this.button1);
+            this.group1.Label = "group1";
+            this.group1.Name = "group1";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Label = "comboBox1";
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Text = null;
+            // 
+            // button1
+            // 
+            this.button1.Label = "button1";
+            this.button1.Name = "button1";
+            this.button1.Click += new Microsoft.Office.Tools.Ribbon.RibbonControlEventHandler(this.button1_Click);
+            // 
+            // editBox1
+            // 
+            this.editBox1.Label = "editBox1";
+            this.editBox1.Name = "editBox1";
+            this.editBox1.Text = "3";
+            // 
+            // Ribbon1
+            // 
+            this.Name = "Ribbon1";
+            this.RibbonType = "Microsoft.Excel.Workbook";
+            this.Tabs.Add(this.tab1);
+            this.Load += new Microsoft.Office.Tools.Ribbon.RibbonUIEventHandler(this.Ribbon1_Load);
+            this.tab1.ResumeLayout(false);
+            this.tab1.PerformLayout();
+            this.group1.ResumeLayout(false);
+            this.group1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        internal Microsoft.Office.Tools.Ribbon.RibbonTab tab1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonGroup group1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonButton button1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonComboBox comboBox1;
+        internal Microsoft.Office.Tools.Ribbon.RibbonEditBox editBox1;
+    }
+
+    partial class ThisRibbonCollection
+    {
+        internal Ribbon1 Ribbon1
+        {
+            get { return this.GetRibbon<Ribbon1>(); }
+        }
+    }
+}
---
+++ Listening/Ribbon1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+using System.Speech.Synthesis;
+using System.Threading;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace Listening
+{
+    public partial class Ribbon1
+    { 
+        public SpeechSynthesizer synth = new SpeechSynthesizer();
+        private void Ribbon1_Load(object sender, RibbonUIEventArgs e)
+        {
+            foreach (var item in synth.GetInstalledVoices())
+            {
+                RibbonDropDownItem ribbonDropDownItemImpl = this.Factory.CreateRibbonDropDownItem();
+                ribbonDropDownItemImpl.Label = item.VoiceInfo.Name;
+                comboBox1.Items.Add(ribbonDropDownItemImpl);
+                comboBox1.Text = item.VoiceInfo.Name;
+            }
+        }
+
+        private void button1_Click(object sender, RibbonControlEventArgs e)
+        {
+            ParameterizedThreadStart ParStart = new ParameterizedThreadStart(Speech);
+            Thread t = new Thread(ParStart);
+            synth.SelectVoice(comboBox1.Text);
+            t.Start(editBox1.Text);
+        }
+
+        private void Speech(object param)
+        {
+            Excel.Application ExApp = Globals.ThisAddIn.Application as Excel.Application;
+            Excel.Range SelectedRange = ExApp.Selection as Excel.Range;
+            foreach (Excel.Range rng in SelectedRange)
+            {
+                int count = Convert.ToInt32(param);
+                if (rng.Column == 5 || rng.Column == 4)
+                {
+                    synth.SelectVoice("Microsoft Huihui Desktop");
+                    count = 1;
+                }
+                for (int i = 0; i < count; i++)
+                {
+                    synth.Speak(rng.Text);
+                }
+            }
+        }
+
+    }
+
+}
+
